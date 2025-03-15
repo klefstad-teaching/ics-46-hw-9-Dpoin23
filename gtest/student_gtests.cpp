df5d607b93e9a7dbf98ea4fddbcf522238dc28df
David Poindexter <davidpoindexter03@gmail.com>
2025-03-15 20:33:17 +0000

    gtests

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,10 +3,13 @@
 #include "../src/dijkstras.h"
 #include "../src/ladder.h"
 
-TEST(Dijkstras, Test) {
-  EXPECT_TRUE(true);
-}
-
 TEST(Ladder, Test) {
-  EXPECT_TRUE(true);
+  set<string> word_list;
+  load_words(word_list, "words.txt");
+  EXPECT_TRUE(generate_word_ladder("cat", "dog", word_list).size() == 4);
+  EXPECT_TRUE(generate_word_ladder("marty", "curls", word_list).size() == 6);
+  EXPECT_TRUE(generate_word_ladder("code", "data", word_list).size() == 6);
+  EXPECT_TRUE(generate_word_ladder("work", "play", word_list).size() == 6);
+  EXPECT_TRUE(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+  EXPECT_TRUE(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
