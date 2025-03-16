f96f20ccb1d31c5019e47245a2ee9e8449430caa
David Poindexter <davidpoindexter03@gmail.com>
2025-03-16 06:33:45 +0000

    syntax update

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -19,7 +19,7 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     for (int i = 1; i <= len2; ++i) {
         for (int j = 1; j <= len1; ++j) {
             if (str1[i-1] == str2[j-i])
-                continue
+                continue;
             else {
                 ++cost;
             }
