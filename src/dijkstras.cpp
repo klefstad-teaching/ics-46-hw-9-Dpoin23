ba590f6e97dde0e998c9c3d1ae4e51e2e1b4041a
David Poindexter <davidpoindexter03@gmail.com>
2025-03-16 01:02:51 +0000

    dijkstra shortest path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -2,14 +2,35 @@
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     int n = G.numVertices;
+    vector<int> distances(n, INF);
+    previous.assign(n, -1);
 
+    using Node = pair<int, int>;
     priority_queue<Node, vector<Node>, greater<Node>> pq;
 
+    distances[source] = 0;
+    pq.push({0. source});
 
     while (!pq.empty()) {
-        Node current = pq.top();
+        int current = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
 
+        if (current > distances[u]) continue;
+
+        for (const Edge& e : G[u]) {
+            int v = e.dst;
+            int weight = e.weight;
+
+            if (distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                pq.push(distances[v], v);
+            }
+        }
     }
+
+    return distances;
 }
 
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
@@ -27,7 +48,7 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
 
 void print_path(const vector<int>& v, int total) {
     cout << "Total cost is " << total << endl;
-    for (int i : v) 
-        cout << i << " ";
+    for (int i = 0; i < v.size(); ++i) 
+        cout << v[i] << " ";
     cout << endl;
 }
